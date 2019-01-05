159cb45f648f7bc410aae5c47c8304405a784219
Cagri Esen <esencgr@gmail.com>
2019-01-05 13:37:14 +0300

    add projects

--- new.cpp
+++ new.cpp
@@ -1,10 +1,28 @@
+
 #include<iostream>
+#include<math.h>
 using namespace std;
-int main (){
-  int number = 9;
-  int a[9] = {0};
-     for (int i=0; i<=number; i++){
-          a[i] = i;
-          cout << a[i] << " ";
-     }
+int main(){
+
+float value,sum=0.0,avarage;
+int count=0;
+
+	cout << "enter integers (exit 9999):";
+	cin >> value ;
+
+	while (value!=9999){
+      float value,sum=0.0,avarage;
+      int count=0;
+	    cout << "enter integers (exit 9999):";
+	    cin >> value ;
+	         while (value!=9999){
+	              sum += value;
+	              count++;
+	              cout << "enter integers (exit 9999):";
+	              cin >> value ;
+
+	          }
+  }
+avarage = sum/count;
+cout<< "avarage:"<< (float)avarage;
 }
