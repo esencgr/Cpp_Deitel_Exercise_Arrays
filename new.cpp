66ff10dfc73e3c242b5ef314209db79e34195faf
Cagri Esen <esencgr@gmail.com>
2019-01-05 13:25:48 +0300

    add projects

---
+++ new.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main (){
+  int number = 9;
+  int a[9] = {0};
+     for (int i=0; i<=number; i++){
+          a[i] = i;
+          cout << a[i] << " ";
+     }
+}
