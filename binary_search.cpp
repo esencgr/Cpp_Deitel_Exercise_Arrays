4ad1152506717e72b4c9bac54b1e1669a796fe8d
Cagri Esen <esencgr@gmail.com>
2019-01-14 16:05:19 +0300

    add file

--- binary_search.cpp
+++ binary_search.cpp
@@ -9,25 +9,28 @@ void line_print(const int[], int, int, int);
 int main (){
    int a[SIZE], search_value, result;
 
-   for (int i=0; i <= SIZE-1; i++)
+   for (int i=0; i <= SIZE-1; i++)   // create input data
        a[i] = i * 2;
 
-   cout << "enter a number between 0-28 : ";
+   cout << "enter a number between 0-28 : ";  // enter the searched number
    cin >> search_value;
 
    head_printing();
    result = binary(a, search_value, 0, SIZE-1);
 
+/* this part is check the finding value and when it is finded write indeces*/
    if (result == -1)
      cout << "the searched value is not find " << endl;
    else
-     cout << "searched value " << search_value << " is finding in " << result << '\n';
+     cout << "searched value " << search_value << " is finding in "
+     << result << '\n';
 
 return 0;
 }
 
-void head_printing (void){
+/* head information and indeces number is writing */
 
+void head_printing (void){
   cout << "indeces" << endl;
 
   for (int i=0; i<=SIZE-1; i++)
@@ -39,25 +42,31 @@ void head_printing (void){
   cout << endl;
 }
 
+/* Calculate medium value and comparing searched value.
+   And changing lenght of array data length is this compare
+*/
+
 int binary (const int b[], int value, int down, int up){
 
     int medium;
 
     while (down <= up){
-      medium = (up + down) / 2;
+       medium = (up + down) / 2;
+       line_print (b, down, medium, up);
 
-      line_print (b, down, medium, up);
-
-      if (value == b[medium])
+       if (value == b[medium])
          return medium;
-      else if (value < b[medium])
+       else if (value < b[medium])
          up = medium - 1;
-      else
+       else
          down = medium + 1;
     }
 
   return -1;
 }
+
+/* writing all comparing result in this part */
+
 void line_print (const int b[], int down, int medium, int up){
 
    for (int i=0; i<=SIZE-1; i++){
