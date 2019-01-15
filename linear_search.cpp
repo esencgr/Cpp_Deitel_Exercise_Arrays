b81ada96959deec94a1b6700541ebe8ade60f8a8
Cagri Esen <esencgr@gmail.com>
2019-01-15 13:46:15 +0300

    add file

---
+++ linear_search.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#define SIZE 10
+using namespace std;
+
+int lineer (const int[], int, int);
+
+int main(){
+     int array[ SIZE ], number, indices;
+
+     cout << "array : ";
+     for (int x=0;  x<=SIZE;  x++){
+         array[x] = x * 2;
+         cout << " " << array[x];
+     }
+
+     cout << "\nEnter the searched number : ";
+     cin >> number;
+
+     indices = lineer (array, number, SIZE);
+
+     if (indices != -1)
+        cout << " searched number is finding " << indices << ". indeces\n";
+     else
+        cout << " searched value not finding \n";
+}
+
+int lineer (const int a[], int search, int size){
+    for (int i=0;  i<=size-1; i++)
+        if ( a[i] == search )
+           return i;
+    return -1;
+}
