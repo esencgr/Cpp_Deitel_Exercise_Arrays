8e6e99902afda5330dd7bc9ac88c31ec5489d35c
Cagri Esen <esencgr@gmail.com>
2019-06-20 13:44:22 +0300

    add file

--- awork.cpp
+++ awork.cpp
@@ -9,7 +9,6 @@ using namespace std;
 int rand_number( void );
 bool is_valid( int );
 void compare( int, int );
-int correct_value ( int, int );
 
 int main(){
 
@@ -18,37 +17,36 @@ int main(){
     cout << "---------------------GENERATING INITIAL VALUES ---------------------- \n";
 
     int user1, software1, user2, software2;
-    cout << "\n--> Computer is generating a number for user..\n";
+    cout << "\n Computer is generating a number for user..\n";
     do{
         software1 = rand_number();
     } while (!is_valid ( software1 ));
 
-    cout << "\n\n--> User is generating a number for computer.. \n";
+    cout << "\n\n User is generating a number for computer.. \n";
     do{
         cout << " User's generated number : ";
         cin >> user2;
     } while (!is_valid ( user2 ));
 
-    cout << "\n\n ------------------- NEW VALUES & INFO --------------------- ";
+    cout << "\n\n ------------------- GUESSING & INFORMATION --------------------- ";
 
-    int correct1 = 0, correct2 = 0;
     while ( 1 ){
 
-        cout << "\n\n--> User is guessing computers number..\n";
+        cout << "\n\n User is guessing computers number..\n";
         do{
            cout << " User's valid number : ";
            cin >> user1;
-        } while (!is_valid ( user1));
+        } while ( !is_valid ( user1) );
 
         cout << " Users information code : ";
         compare( software1, user1 );
 
-        if ( software1 == user1){
+        if ( software1 == user1 ){
            cout << "\n\n ------ THE NUMBER IS => " << user1 << " ------> " << " USER WIN THE GAME!"<< endl;
            break;
         }
 
-        cout << "\n\n--> Computer is guessing users number..\n";
+        cout << "\n\n Computer is guessing users number..\n";
         do{
             software2 = rand_number();
             cout << " Computer valid number : " << software2 << endl;
@@ -92,43 +90,3 @@ bool is_valid(int number){
    }
    return 1;
 }
-
-/*
-This test function is the compare the users and computers number, and if they
-include same digit, the function assignments array's element 1 value, 0 if not.
-*/
-void compare ( int num1, int num2 ){
-    int count [ 3 ] = { 0 }, sum = 0;
-    for (int i = 2;  i >= 0;  i--) {
-        int x = pow ( 10, i );
-        int temp1 = num1 / x;
-        int temp2 = num2 / x;
-            if ( temp1 == temp2 )
-               count[i] = 1;
-        num1 %= x;
-        num2 %= x;
-        cout << count [i];
-    }
-}
-/*
-This function is checking the two integers and if includes same digits ( it's mean
-element value is 1 ) sum vaariable is increase by 1.
-*/
-// int correct_value ( int num1, int num2 ){
-//     int count [ 3 ] = { 0 }, sum = 0;
-//     for (int i = 2;  i >= 0;  i--) {
-//         int x = pow ( 10, i );
-//         int temp1 = num1 / x;
-//         int temp2 = num2 / x;
-//             if ( temp1 == temp2 )
-//                count[ i ] = 1;
-//         num1 %= x;
-//         num2 %= x;
-//         sum += count[ i ];
-//     }
-//     return sum;
-// }
-// correct2 = correct_value( software2, user2 );
-// cout << "\n Corectivity : " << correct2;
-// correct1 = correct_value( software1, user1 );
-// cout << "\n Corectivity : " << correct1;
